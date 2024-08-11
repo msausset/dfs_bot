3152cbfd40e320a9761c5dfb8a096bfdab94c990
msausset <maxime.sausset@hotmail.fr>
2024-08-12 01:38:34 +0200

    Changement port app.py

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:3000"])
@@ -62,4 +63,4 @@ def clear_resources():
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
